44187b6d06752d147a75c820ae546b7a92b6b175
Mario Fernández <57534141+mariofergon99@users.noreply.github.com>
2023-01-26 12:34:08 +0100

    bbbbbbbbbbbbbbbbbb

--- prueba.py
+++ prueba.py
@@ -21,7 +21,7 @@ with DAG("prueba", start_date=datetime(2021, 1 ,1),
     
     prueba_spark = SparkSubmitOperator(
             task_id="prueba",
-            application="/home/airflow/.local/lib/python3.7/site-packages/pyspark/examples/src/main/python",
+            application="/home/airflow/.local/lib/python3.7/site-packages/pyspark/examples/src/main/python/pi.py",
             conn_id="spark_conn",
             verbose=False
         )
