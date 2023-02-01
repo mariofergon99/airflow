119c69459614480f57467af73a206ae5b7c3f42e
clm448 <72398080+clm448@users.noreply.github.com>
2023-02-01 12:05:24 +0100

    Add files via upload

--- dag_prueba.py
+++ dag_prueba.py
@@ -1,5 +1,5 @@
 from airflow import DAG
-# from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
 
@@ -20,21 +20,21 @@ default_args = {
 with DAG("prueba_Bash_2", start_date=datetime(2021, 1 ,1), 
     schedule_interval="@daily", default_args=default_args, catchup=False) as dag:
     
-    # prueba_spark = SparkSubmitOperator(
-    #         task_id="prueba_spark",
-    #         application="/home/airflow/.local/lib/python3.7/site-packages/pyspark/examples/src/main/python/pi.py",
-    #         conn_id="spark_conn",
-    #         verbose=False
-    #     )
+    prueba_spark = SparkSubmitOperator(
+            task_id="prueba_spark",
+            application="/home/airflow/.local/lib/python3.8/site-packages/pyspark/examples/src/main/python/pi.py",
+            conn_id="spark_conn",
+            verbose=False
+        )
 
     prueba_python = PythonOperator(
         task_id="prueba_python",
         python_callable=dummy
     )
 
-    prueba_bash = BashOperator(
-        task_id="prueba_bash",
-        bash_command="""curl -X POST -H "Content-Type: application/json" -d '{"appResource":"/opt/bitnami/spark/examples/src/main/python/pi.py", "sparkProperties": {"spark.driver.memory": "1g","spark.master": "spark://spark-master-svc.default.svc:7077","spark.app.name": "Prueba bash operator con API","spark.submit.deployMode": "cluster","spark.ui.showConsoleProgress": "true","spark.eventLog.enabled": "false","spark.logConf": "false","spark.executor.cores":"1","spark.cores.max":"1","spark.driver.bindAddress": "0.0.0.0"}, "clientSparkVersion": "3.3.0", "mainClass": "org.apache.spark.deploy.SparkSubmit","environmentVariables": { "SPARK_YARN_MODE": true}, "action": "CreateSubmissionRequest","appArgs": ["/opt/bitnami/spark/examples/src/main/python/pi.py"]}' http://spark-master-svc.default.svc:6066/v1/submissions/create"""
-    )
+    # prueba_bash = BashOperator(
+    #     task_id="prueba_bash",
+    #     bash_command="""curl -X POST -H "Content-Type: application/json" -d '{"appResource":"/opt/bitnami/spark/examples/src/main/python/pi.py", "sparkProperties": {"spark.driver.memory": "1g","spark.master": "spark://spark-master-svc.default.svc:7077","spark.app.name": "Prueba bash operator con API","spark.submit.deployMode": "cluster","spark.ui.showConsoleProgress": "true","spark.eventLog.enabled": "false","spark.logConf": "false","spark.executor.cores":"1","spark.cores.max":"1","spark.driver.bindAddress": "0.0.0.0"}, "clientSparkVersion": "3.3.0", "mainClass": "org.apache.spark.deploy.SparkSubmit","environmentVariables": { "SPARK_YARN_MODE": true}, "action": "CreateSubmissionRequest","appArgs": ["/opt/bitnami/spark/examples/src/main/python/pi.py"]}' http://spark-master-svc.default.svc:6066/v1/submissions/create"""
+    # )
 
-    prueba_bash >> prueba_python
+    prueba_spark >> prueba_python
